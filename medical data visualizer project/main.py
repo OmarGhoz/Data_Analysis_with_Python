d439530cd6fc15f446579e2c8b7f2def67dfc501
Omar Ghoz <133299432+OmarGhoz@users.noreply.github.com>
2024-11-16 01:40:16 +0200

    save changes

---
+++ medical data visualizer project/main.py
@@ -0,0 +1,8 @@
+from medical_data_visualizer import draw_cat_plot, draw_heat_map
+
+#Drawing the plots and saving them
+cat_plot = draw_cat_plot()
+cat_plot.savefig("catplot.png")
+
+heat_map = draw_heat_map()
+heat_map.savefig("heatmap.png")
